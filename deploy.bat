d278a706baa64974ba967f6390a8bd75a594be1e
LuoXiaohei <cn_lshang@qq.com>
2015-05-23 03:08:12 +0800

    Update Home Page

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,6 @@
 git add .
-git commit -a -m %1
+if "%1" == "" (
+    git commit -a -m "Update Home Page") else (
+    git commit -a -m %1)
 git push github master:master
 git push gitcafe master:gitcafe-pages
